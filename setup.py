88af79ca00222c1cacded876240c5e9d322805fb
umairinayat <umairinayat975@gmail.com>
2025-02-24 08:39:43 +0000

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+
+    name="src",
+    version="0.0.1",
+    author="umair",
+    author_email="umairinayat975@gmail.com",
+    packages=find_packages(),
+)
